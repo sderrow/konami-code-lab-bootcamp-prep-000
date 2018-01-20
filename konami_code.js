f81cdff27e8bb3e0574c415da770887ad8213ac7
Sean Derrow <sderrow@mit.edu>
2018-01-20 00:48:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,18 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const body = document.querySelector('body')
-  var i = 0
+  const body = document.querySelector('body');
+  var i = 0;
   body.addEventListener('keydown', function(e) {
     if (e.which === code[i]) {
-      i++
+      i++;
       if (i == code.length) {
-        alert('Congratulations on finding the hidden secret!')
-        i = 0
+        alert('Congratulations on finding the hidden secret!');
+        i = 0;
       }
     } else {
-      i = 0
+      i = 0;
     }
   })
 
